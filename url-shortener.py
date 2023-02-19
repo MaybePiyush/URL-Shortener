8efd41c0b70b118cb59a727c444f36e576c0a4b9
ᴍᴀʏʙᴇ ᴘɪʏᴜꜱʜ <124165177+MaybePiyush@users.noreply.github.com>
2023-02-19 17:17:45 +0530

    Update url-shortener.py

--- url-shortener.py
+++ url-shortener.py
@@ -1,13 +1,12 @@
 import pyshorteners
 from pyperclip import copy
 from pystyle import Write, Colors
-Write.Print("Welcome To URL Shortener\n", Colors.baby_blue, interval=0.0025)
+Write.Print("Welcome To URL Shortener\n", Colors.dark_blue, interval=0.0025)
 while True:
-    url = Write.Input("Enter URL: ", Colors.dap_blue, interval=0.025)
+    url = Write.Input("Enter URL: ", Colors.dark_blue, interval=0.025)
     shortener = pyshorteners.Shortener()
     short_url = shortener.tinyurl.short(url)
-    copy( short_url)
-    Write.Print(f"Shortened URL: {short_url}\nCopied short url to clipboard\n", Colors.dap_blue, interval=0.025)
+    Write.Print(f"Shortened URL: {short_url}\nCopied short url to clipboard\n", Colors.dark_blue, interval=0.025)
     if url in ("exit", "quit", "q", "e"):
-        Write.Print("Exiting...", Colors.baby_blue, interval=0.025)
+        Write.Print("Exiting...", Colors.dark_blue, interval=0.025)
         break
